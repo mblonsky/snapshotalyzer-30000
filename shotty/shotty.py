086d6d100bef6527514559d4b92f4f64b1447be2
mblonsky <mblonsky@orion.mblonsky.com>
2019-07-10 21:35:57 -0700

    List volumes and snapshots. Naive implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,9 +16,78 @@ def filter_instances(project):
 	return instances
 
 @click.group()
+def cli():
+	"""Shotty manages snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+	"""Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+	help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+	"List EC2 instances"
+
+	instances = filter_instances(project)
+
+	for i in instances:
+		for v in i.volumes.all():
+			for s in v.snapshots.all():
+				print(", ".join((
+					s.id,
+					v.id,
+					i.id,
+					s.state,
+					s.progress,
+					s.start_time.strftime("%c")
+				)))
+
+	return
+@cli.group('volumes')
+def volumes():
+	"""Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+	help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+	"List EC2 instances"
+
+	instances = filter_instances(project)
+
+	for i in instances:
+		for v in i.volumes.all():
+			print(", ".join((
+			v.id,
+			i.id,
+			v.state,
+			str(v.size) + "GiB",
+			v.encrypted and "Encrypted" or "Not Encrypted"
+			)))
+
+	return
+
+@cli.group()
 def instances():
 	"""Commands for instances"""
 
+@instances.command('snapshot',
+	help="Create snapshots ofall volumes")
+@click.option('--project', default=None,
+	help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+	"Create snapshots for EC2 instances"
+
+	instances = filter_instances(project)
+
+	for i in instances:
+		i.stop()
+		for v in i.volumes.all():
+			print("Creating snapshot of {0}".format(v.id))
+			v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+
+	return
 @instances.command('list')
 @click.option('--project', default=None,
 	help="Only instances for project (tag Project:<name>)")
@@ -69,4 +138,4 @@ def start_instances(project):
 	return
 
 if __name__ == '__main__':
-	instances()
+	cli()
